e03224b04ef1550e0cf6b21b0dbd7e34c7434023
Nguyễn Minh Trí <133070397+TriNM2003@users.noreply.github.com>
2025-06-15 15:21:11 +0700

    Update model.js

--- src/models/index.js
+++ src/models/index.js
@@ -43,4 +43,14 @@ const db = {
   Species,
 };
 
+db.connectDB = async () => {
+    try {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Connected to MongoDB");
+    } catch (err) {
+      console.error("MongoDB connection error:", err);
+      process.exit(1); // Kết thúc quá trình nếu có lỗi
+    }
+  };
+
 module.exports = db;
