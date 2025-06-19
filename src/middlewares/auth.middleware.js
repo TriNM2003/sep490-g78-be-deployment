c9df01f0cde39dd359e30f233ce32cbc7f8d018e
lehoan411 <notlehoan411@gmail.com>
2025-06-19 09:48:22 +0700

    feat(add auth middleware)

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const createError = require("http-errors");
+const passport = require("passport");
+
+const verifyAccessToken = (req, res, next) => {
+    if (!req.headers['authorization']) {
+        return next(createError.Unauthorized)
+    }
+    const authHeader = req.headers['authorization']
+    const bearerToken = authHeader.split(' ')
+    const token = bearerToken[1];
+
+    if (!token) {
+        throw createError.NotFound("Token is not provided!")
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+        if (err) {
+            const message = err.name == 'JsonWebTokenError' ? 'Unauthorized' : err.message;
+            return next(createError.Unauthorized(message))
+        }
+        req.payload = payload;
+        next();
+    })
+};
+
+const verifyGoogleCallback = passport.authenticate("google", { failureRedirect: "http://localhost:5173/error" });
+
+const authMiddleware = {
+    verifyAccessToken,
+    verifyGoogleCallback,
+}
+
+module.exports = authMiddleware;
