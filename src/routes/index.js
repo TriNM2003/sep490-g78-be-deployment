38a93ae768f9b645f8992b4958b1d485bb490df9
TunDQ <tuandq.work@gmail.com>
2025-06-19 12:04:45 +0700

    feat: login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,8 @@
-module.exports = {
-    
-}
+const express = require("express");
+const authRouter = require("./auth.route");
+
+const router = express.Router();
+
+router.use("/auth", authRouter); // chỉ mount một lần
+
+module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,43 +2,49 @@ const morgan = require("morgan");
 const express = require("express");
 const bodyParser = require("body-parser");
 const httpsErrors = require("http-errors");
-const cors = require("cors"); 
+const cors = require("cors");
 require("dotenv").config();
-
+const allRoutes = require("./routes");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+require("./configs/passport");
 
 const path = require("path");
 
 const http = require("http");
 const db = require("./models");
 
-
 const app = express();
 
 // Sử dụng cors middleware để cho phép request từ localhost:3000
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:3000"],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(passport.initialize());
 
-
-
+app.use("/api", allRoutes);
 app.get("/", async (req, res, next) => {
   res.status(200).json({ message: "Server is running" });
 });
 
 // Định tuyến theo các chức năng thực tế
 
-
 app.use(async (req, res, next) => {
   next(httpsErrors(404, "Bad Request"));
 });
 app.use(async (err, req, res, next) => {
   resStatus = err.status || 500;
-  return res.status(resStatus||500).json({ error: { status: err.status, message: err.message } });
+  return res
+    .status(resStatus || 500)
+    .json({ error: { status: err.status, message: err.message } });
 });
 
 const host = process.env.HOSTNAME;
