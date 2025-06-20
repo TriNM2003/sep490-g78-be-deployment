657fbe860010bfe76658f0214a141f3fa25d41a2
HaoTH <haothhe176192@fpt.edu.vn>
2025-06-20 15:06:35 +0700

    feat:edit profile- change password

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,6 @@ const authController = require("./auth.controller");
 const userController = require('./user.controller');
 
 module.exports = {
-    authController
+    authController,
   userController,
 };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -3,17 +3,19 @@ const userRouter = express.Router();
 const bodyParser = require("body-parser");
 const { userController } = require("../controllers");
 const cloudinary = require("../configs/cloudinary");
+const { verifyAccessToken, verifyGoogleCallback, verifyGoogleCallbackAdmin } = require("../middlewares/auth.middleware");
 
 userRouter.use(bodyParser.json());
 
 userRouter.get("/", userController.getAllUsers);
-userRouter.get("/:userId", userController.getUserById);
-userRouter.put("/:userId/change-password", userController.changePassword);
-userRouter.put("/:userId/edit-profile",
+userRouter.get("/user-profile",verifyAccessToken, userController.getUserById);
+userRouter.put("/change-password", verifyAccessToken, userController.changePassword);
+userRouter.put("/edit-profile",
   cloudinary.upload.fields([
     { name: "avatar", maxCount: 1 },
     { name: "background", maxCount: 1 },
   ]),
+  verifyAccessToken,
   userController.editProfile
 );
 
