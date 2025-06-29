cbe56c3c4a5a0d6bc1b4f63079b3c44008b34600
Nguyễn Minh Trí <133070397+TriNM2003@users.noreply.github.com>
2025-06-29 09:42:38 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,7 +11,7 @@ module.exports = {
     userRouter,
     authRouter,
     adoptionSubmissionRouter,
-    medicalRecordRouter
+    medicalRecordRouter,
     shelterRouter,
     adoptionTemplateRouter,
 }
