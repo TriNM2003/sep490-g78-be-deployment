5ee75ea1214dd21feb26a9f799ad0a0b5ed680bf
TunDQ <tuandq.work@gmail.com>
2025-07-03 21:08:09 +0700

    feat: shelter dasboard api

--- src/controllers/shelter.controller.js
+++ src/controllers/shelter.controller.js
@@ -2,107 +2,145 @@ const shelterService = require("../services/shelter.service");
 
 //USER
 const getShelterRequestByUserId = async (req, res, next) => {
-    try {
-        const {id} = req.payload;
-        const response = await shelterService.getShelterRequestByUserId(id);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-const sendShelterEstablishmentRequest = async (req, res, next) => {
-    try {
-        const {id} = req.payload;
-        const requestData = req.body;
-        const response = await shelterService.sendShelterEstablishmentRequest(id, requestData, req.files);
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-const getShelterProfile = async (req, res, next) => {
   try {
-        const {shelterId} = req.params;
-        const response = await shelterService.getShelterProfile(shelterId);
-        res.status(200).json(response);
-  } catch (error) {
-    res.status(404).json({ message: error.message });
-  }
-}
-
-const editShelterProfile = async (req, res, next) => {
-  try {
-        const {shelterId} = req.params;
-        const updatedData = {
-            ...req.body,
-            avatar: req.files?.avatar?.[0],
-            background: req.files?.background?.[0],
-        };
-        const response = await shelterService.editShelterProfile(shelterId, updatedData);
-        res.status(200).json(response);
+    const { id } = req.payload;
+    const response = await shelterService.getShelterRequestByUserId(id);
+    res.status(200).json(response);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
 };
-async function getAll(req, res,next) {
-    try {
-        const shelters = await shelterService.getAll();
-        res.status(200).json(shelters);
-      } catch (error) {
-        res.status(400).json({ message: error.message });
-      }
+const sendShelterEstablishmentRequest = async (req, res, next) => {
+  try {
+    const { id } = req.payload;
+    const requestData = req.body;
+    const response = await shelterService.sendShelterEstablishmentRequest(
+      id,
+      requestData,
+      req.files
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+const getShelterProfile = async (req, res, next) => {
+  try {
+    const { shelterId } = req.params;
+    const response = await shelterService.getShelterProfile(shelterId);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+const editShelterProfile = async (req, res, next) => {
+  try {
+    const { shelterId } = req.params;
+    const updatedData = {
+      ...req.body,
+      avatar: req.files?.avatar?.[0],
+      background: req.files?.background?.[0],
+    };
+    const response = await shelterService.editShelterProfile(
+      shelterId,
+      updatedData
+    );
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+async function getAll(req, res, next) {
+  try {
+    const shelters = await shelterService.getAll();
+    res.status(200).json(shelters);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 }
 
+// Dashboard statistics
+const getShelterDashboardStatistics = async (req, res) => {
+  try {
+    const { shelterId } = req.params;
+    const caringPets = await shelterService.getShelterCaringPetsCount(
+      shelterId
+    );
+    const adoptedPets = await shelterService.getShelterAdoptedPetsCount(
+      shelterId
+    );
+    const posts = await shelterService.getShelterPostsCount(shelterId);
+    const members = await shelterService.getShelterMembersCount(shelterId);
+    const petGrowth = await shelterService.getShelterPetGrowthByMonth(
+      shelterId
+    );
+
+    res.status(200).json({
+      caringPets,
+      adoptedPets,
+      posts,
+      members,
+      petGrowth,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
 
 // ADMIN
 const getAllShelter = async (req, res, next) => {
-    try {
-        const shelters = await shelterService.getAllShelter();
-        res.status(200).json(shelters);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const shelters = await shelterService.getAllShelter();
+    res.status(200).json(shelters);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 const getAllShelterEstablishmentRequests = async (req, res, next) => {
-    try {
-        const shelters = await shelterService.getAllShelterEstablishmentRequests();
-        res.status(200).json(shelters);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const shelters = await shelterService.getAllShelterEstablishmentRequests();
+    res.status(200).json(shelters);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 const getOverviewStatistic = async (req, res, next) => {
-    try {
-        const overviewStatistics = await shelterService.getOverviewStatistic();
-        res.status(200).json(overviewStatistics);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
+  try {
+    const overviewStatistics = await shelterService.getOverviewStatistic();
+    res.status(200).json(overviewStatistics);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 const reviewShelterEstablishmentRequest = async (req, res, next) => {
-    try {
-        const {requestId, decision, rejectReason} = req.body;
-        const response = await shelterService.reviewShelterEstablishmentRequest({requestId, decision, rejectReason});
-        res.status(200).json(response);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
-}
-
+  try {
+    const { requestId, decision, rejectReason } = req.body;
+    const response = await shelterService.reviewShelterEstablishmentRequest({
+      requestId,
+      decision,
+      rejectReason,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
 
 const shelterController = {
-    //USER
-    sendShelterEstablishmentRequest,
-    getShelterRequestByUserId,
-    getShelterProfile,
-    editShelterProfile,
-    getAll,
+  //USER
+  sendShelterEstablishmentRequest,
+  getShelterRequestByUserId,
+  getShelterProfile,
+  editShelterProfile,
+  getAll,
+  getShelterDashboardStatistics,
 
-    //ADMIN
-    getAllShelter,
-    getAllShelterEstablishmentRequests,
-    getOverviewStatistic,
-    reviewShelterEstablishmentRequest,
+  //ADMIN
+  getAllShelter,
+  getAllShelterEstablishmentRequests,
+  getOverviewStatistic,
+  reviewShelterEstablishmentRequest,
 };
 
 module.exports = shelterController;
--- src/routes/shelter.route.js
+++ src/routes/shelter.route.js
@@ -5,21 +5,32 @@ const shelterController = require("../controllers/shelter.controller");
 const { verifyAccessToken } = require("../middlewares/auth.middleware");
 const { isAdmin } = require("../middlewares/admin.middleware");
 const cloudinary = require("../configs/cloudinary");
-const { isShelterManager } = require("../middlewares/shelterManager.middleware");
+const {
+  isShelterManager,
+} = require("../middlewares/shelterManager.middleware");
 
 shelterRouter.use(bodyParser.json());
 
 // USER
-shelterRouter.post("/send-shelter-request",
-    verifyAccessToken, 
-    cloudinary.upload.fields([{ name: "shelterLicense", maxCount: 1 }]), 
-    shelterController.sendShelterEstablishmentRequest)
-shelterRouter.get("/get-shelter-request", verifyAccessToken, shelterController.getShelterRequestByUserId);
-shelterRouter.get("/get-profile/:shelterId", 
-    [verifyAccessToken, isShelterManager],
-    shelterController.getShelterProfile);
-shelterRouter.put("/edit-profile/:shelterId",
-    [verifyAccessToken, isShelterManager],
+shelterRouter.post(
+  "/send-shelter-request",
+  verifyAccessToken,
+  cloudinary.upload.fields([{ name: "shelterLicense", maxCount: 1 }]),
+  shelterController.sendShelterEstablishmentRequest
+);
+shelterRouter.get(
+  "/get-shelter-request",
+  verifyAccessToken,
+  shelterController.getShelterRequestByUserId
+);
+shelterRouter.get(
+  "/get-profile/:shelterId",
+  [verifyAccessToken, isShelterManager],
+  shelterController.getShelterProfile
+);
+shelterRouter.put(
+  "/edit-profile/:shelterId",
+  [verifyAccessToken, isShelterManager],
   cloudinary.upload.fields([
     { name: "avatar", maxCount: 1 },
     { name: "background", maxCount: 1 },
@@ -28,13 +39,33 @@ shelterRouter.put("/edit-profile/:shelterId",
 );
 shelterRouter.get("/get-all", shelterController.getAll);
 
-
-
 // ADMIN
-shelterRouter.get("/admin/get-shelters-list", [verifyAccessToken, isAdmin], shelterController.getAllShelter);
-shelterRouter.get("/admin/get-shelter-requests-list", [verifyAccessToken, isAdmin], shelterController.getAllShelterEstablishmentRequests);
-shelterRouter.get("/admin/get-overview-statistics", [verifyAccessToken, isAdmin], shelterController.getOverviewStatistic);
-shelterRouter.post("/admin/review-shelter-request", [verifyAccessToken, isAdmin], shelterController.reviewShelterEstablishmentRequest);
+shelterRouter.get(
+  "/admin/get-shelters-list",
+  [verifyAccessToken, isAdmin],
+  shelterController.getAllShelter
+);
+shelterRouter.get(
+  "/admin/get-shelter-requests-list",
+  [verifyAccessToken, isAdmin],
+  shelterController.getAllShelterEstablishmentRequests
+);
+shelterRouter.get(
+  "/admin/get-overview-statistics",
+  [verifyAccessToken, isAdmin],
+  shelterController.getOverviewStatistic
+);
+shelterRouter.post(
+  "/admin/review-shelter-request",
+  [verifyAccessToken, isAdmin],
+  shelterController.reviewShelterEstablishmentRequest
+);
 
+// Thống kê dashboard shelter
+shelterRouter.get(
+  "/dashboard-statistics/:shelterId",
+  [verifyAccessToken, isShelterManager],
+  shelterController.getShelterDashboardStatistics
+);
 
 module.exports = shelterRouter;
